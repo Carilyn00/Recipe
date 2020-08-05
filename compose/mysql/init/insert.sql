a934c5de9ec7621c1ade93a4acd75112ceddab78
Carilyn00 <62640850+Carilyn00@users.noreply.github.com>
2020-08-05 17:19:35 +0800

    Create insert.sql

---
+++ compose/mysql/init/insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO recipe VALUES (123, 'Diced up the Onions', 5, 'Beginner', 'Fried Onions', 10, 2);
+INSERT INTO recipe VALUES (140, 'Diced up the Tomato', 5, 'Beginner', 'Boiled Tomato', 10, 2);
+INSERT INTO recipe VALUES (140, 'Put it into the air fryer', 5, 'Advanced', 'Groza', 10, 2);
+INSERT INTO recipe VALUES (140, 'Wrap everything', 5, 'Advanced', 'Buritto', 10, 2);
+INSERT INTO recipe VALUES (140, 'Pack everything', 5, 'Beginner', 'Sandwich', 10, 2);
+
+INSERT INTO category VALUES (234, 'French');
+INSERT INTO category VALUES (345, 'Vegan');
+INSERT INTO category VALUES (456, 'Italian');
+INSERT INTO category VALUES (567, 'Mexican');
+INSERT INTO category VALUES (678, 'American');
+
